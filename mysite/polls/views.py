6b86ebdab9c5bd7014843bc28244aac9a7b95e22
cjoseph246 <cjoseph246@gmail.com>
2016-05-04 14:27:39 -0400

    formatting

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -9,20 +9,20 @@ from .models import Choice, Question
 
 
 class IndexView(generic.ListView):
-	template_name = 'polls/index.html'
-	context_object_name = 'latest_question_list'
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-	def get_queryset(self):
-		'''
-		Return the last five published questions, but not future ones.
-		'''
-		return Question.objects.filter(pub_date__lte=timezone.now()).\
-			order_by('-pub_date')[:5]
+    def get_queryset(self):
+        '''
+        Return the last five published questions, but not future ones.
+        '''
+        return Question.objects.filter(pub_date__lte=timezone.now()).\
+            order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
-	model = Question
-	template_name = 'polls/detail.html'
+    model = Question
+    template_name = 'polls/detail.html'
     
     def get_queryset(self):
         '''Excludes any questions that aren't published yet.'''
@@ -30,8 +30,8 @@ class DetailView(generic.DetailView):
 
 
 class ResultsView(generic.DetailView):
-	model = Question
-	template_name = 'polls/results.html'
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
